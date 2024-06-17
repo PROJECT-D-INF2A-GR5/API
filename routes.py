3941f4ddb15d3f914b272517191a753d378f0b7f
Pieter Landaal <0981786@hr.nl>
2024-06-17 14:21:14 +0200

    user id

--- routes.py
+++ routes.py
@@ -4,4 +4,4 @@ from controller import test_db, conversation, user
 
 api.add_resource(test_db.TestDB, "/testdb/<string:db_type>", methods=["GET"])
 api.add_resource(conversation.Conversation, "/openai/<string:user_id>", methods=["POST", "GET"])
-api.add_resource(user.User, "/user/<string:user>", methods=["POST", "GET"])
+api.add_resource(user.User, "/user", methods=["POST", "GET"])
